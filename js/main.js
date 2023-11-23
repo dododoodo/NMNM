d18488ff0335a85d15d1a5a7a20f040518f9dbb9
Doyeon Kim <nsa10050@naver.com>
2023-11-24 02:21:04 +0900

    최종이요

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+document.oncontextmenu = function(){return false;}
+
 AOS.init();
 /* 주석부분과 같이 AOS API - Global Setting 가능
 AOS.init({
@@ -45,31 +47,36 @@ window.addEventListener("mousemove", mouseMove);
 
 
 
-
 window.addEventListener('scroll', function() {
     const sec10 = document.getElementById('sec10');
     const color1 = document.getElementById('color1');
     const color2 = document.getElementById('color2');
     const color3 = document.getElementById('color3');
-
+  
     const sec10Top = sec10.getBoundingClientRect().top;
     const screenHeight = window.innerHeight;
-
-if (sec10Top <= screenHeight / 2) {
-    color1.classList.add('animation');
-    setTimeout(function() {
-    color2.classList.add('animation');
-    }, 500);
-    setTimeout(function() {
-    color3.classList.add('animation');
-    }, 1000);
-} else {
-    [color1, color2, color3].forEach(function(element) {
-    element.classList.remove('animation');
-    void element.offsetWidth; // Reflow to restart animation
-    });
-}
-});
+  
+    if (sec10Top <= screenHeight / 2) {
+      color1.classList.remove('hidden');
+      color1.classList.add('animation');
+  
+      setTimeout(function() {
+        color2.classList.remove('hidden');
+        color2.classList.add('animation');
+      }, 500);
+  
+      setTimeout(function() {
+        color3.classList.remove('hidden');
+        color3.classList.add('animation');
+      }, 1000);
+    } else {
+      [color1, color2, color3].forEach(function(element) {
+        element.classList.add('hidden');
+        element.classList.remove('animation');
+      });
+    }
+  });
+  
 
 // modal
 let modal = document.getElementById("myModal");
